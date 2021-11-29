3af42b7e65344517f1cea99e6376b9059a3c0948
Stefano1991 <pieronistefano@hotmail.com>
2021-11-29 18:48:23 -0300

    PlayerMovement
    Creation of the player movement script

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,6 +7,13 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     [SerializeField] Rigidbody2D rb;
+    Vector2 movement;
+
+    #region Mouse Looking Variables
+    [SerializeField] Camera cameraObj;
+    Vector3 dir;
+
+    #endregion
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +24,19 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        //Get the inputs for both Vertical and Horizontal axes and stores them to movement x and y values
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        //Makes the object face towards the mouse position
+        dir = Input.mousePosition - cameraObj.WorldToScreenPoint(transform.position);
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+    private void FixedUpdate()
+    {
+        //Uses the rigidbody to move the object using the movement vector, movespeed and a fixed amount of time so that it moves smoothly
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
